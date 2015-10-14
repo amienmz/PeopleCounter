f0459540d5377eb9e368e734a8b36fdb90005be4
Bui Dac Huy <huybuidac@gmail.com>
2015-10-14 17:48:19 +0700

    stream: using threading

---
+++ member/huy/server/const.py
@@ -0,0 +1,9 @@
+__author__ = 'huybu'
+
+POS_DATA = 0
+POS_ADDRESS = 1
+POS_IP = 0
+POS_PORT = 1
+CMD_CONNECT = 'connect'
+CMD_DISCONNECT = 'disconnect'
+CMD_OK = 'ok'
---
+++ member/huy/server/pi_server.py
@@ -0,0 +1,7 @@
+import threading
+
+__author__ = 'huybu'
+
+class Pi_Server(threading.Thread):
+    def __init__(self):
+        pass
