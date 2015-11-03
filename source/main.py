4068b222d725e4043052e6af9462ffb28458767a
hoangtuan211@gmail.com <Developer211@!>
2015-11-03 05:40:09 -0800

    update

--- source/main.py
+++ source/main.py
@@ -2,6 +2,7 @@ __author__ = 'pc'
 from source.utils.depthmapCalculator import DepthmapCalculator
 from source.utils.videoStreamer import VideoStreamer
 from source.utils.backgroundSubtraction import BackgroundSubtraction
+from source.utils.ObjectMoving import ObjectMoving
 import numpy as np
 import cv2
 
@@ -12,7 +13,7 @@ depthmapCalculator = DepthmapCalculator('../Datas/data')
 calibration = depthmapCalculator.get_calibration()
 block_matcher = depthmapCalculator.get_block_macher()
 backgroundSubtraction = BackgroundSubtraction()
-
+imgObjectMoving = ObjectMoving(150,150,10)
 params = cv2.SimpleBlobDetector_Params()
 params.filterByColor = 1
 params.blobColor = 150
@@ -26,13 +27,15 @@ while True:
     image_left, image_right = streamer.get_image_from_video(videoLeft,videoRight)
     depthmap = depthmapCalculator.calculate(image_left, image_right, block_matcher, calibration)
     # cv2.imshow("depthmap", depthmap)
-    display = backgroundSubtraction.compute(depthmap)
-
+    mask, display = backgroundSubtraction.compute(depthmap)
+    res,pon1,pon2 = imgObjectMoving.getImgObjectMoving(mask)
+    if res:
+        cv2.rectangle(display,pon1, pon2,(255,255,255), 2)
     cv2.imshow("back", display)
     char = cv2.waitKey(10)
     if (char == 99):
-        count += 1
-        cv2.imwrite('capture/img' + str(count)+'.png', display)
+        # count += 1
+        # cv2.imwrite('capture/img' + str(count)+'.png', display)
         cv2.waitKey(0)
     if (char == 27):
         break
