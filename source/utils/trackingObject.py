b5823ef48bb6727693820838c0819f4eb8de102f
hoangtuan211@gmail.com <Developer211@!>
2015-12-06 04:39:17 -0800

    update tracking

--- source/utils/trackingObject.py
+++ source/utils/trackingObject.py
@@ -25,7 +25,9 @@ class TrackingObj(object):
     def remove_track(self):
         for data in self.allObj:
             if data[3] == False:
-                if data[5] < self.maxPass:
+                if self.check_withLine(data[1],data[2]) == None:
+                    self.allObj.remove(data)
+                elif data[5] < self.maxPass:
                     data[5] += 1
                 else:
                     self.allObj.remove(data)
@@ -73,54 +75,14 @@ class TrackingObj(object):
             return 1
 
 
-    # def trackingObj(self,pon1,pon2):
-    #     y, x = [pon1[1],pon1[0]]
-    #     h, w = [pon2[1],pon2[0]]
-    #     # print x,y,w,h
-    #     if len(self.allObj) == 0:
-    #         self.allObj.append([x,y,w,h,True,0,0,None])
-    #         return None
-    #     haveline = False
-    #
-    #     for data in self.allObj:
-    #         if data[0] != None:
-    #             if (data[1] <= y <= data[1]+data[3] and x <= data[0] <= x + w) or (x<=data[0]<=x+w and y <= data[1]<=y+h)or (data[0] <= x <= data[0] + data[2] and y<=data[1]<=y+h) or (data[0] <= x <= data[0]+data[2] and data[1]<=y<=data[1]+data[3]):
-    #                 # Point1 = ((data[0]+data[2])/2,(data[1]+data[3])/2)
-    #                 data[0] = x
-    #                 data[1] = y
-    #                 data[2] = w
-    #                 data[3] = h
-    #                 data[4] = True
-    #                 res, ln = self.sysn_line(data,y,h)
-    #                 if res:
-    #                     inout = self.check_in_out(data,ln)
-    #
-    #                     if inout == 0:
-    #                         self.OutSh +=1
-    #                         self.queue_update_pc.put(const.TYPE_OUT)
-    #                         self.queue_post2web.put(const.TYPE_OUT)
-    #                     elif inout == 1:
-    #                         self.InSh +=1
-    #                         self.queue_update_pc.put(const.TYPE_IN)
-    #                         self.queue_post2web.put(const.TYPE_IN)
-    #                 haveline = True
-    #                 break
-    #     if haveline == False:
-    #         try:
-    #             ins = self.allObj.index([None,None,None,None,False,0,0,None])
-    #             self.allObj.insert(ins,[x,y,w,h,True,0,0,None])
-    #             self.allObj.remove(self.allObj[ins+1])
-    #         except Exception as e:
-    #             self.allObj.append([x,y,w,h,True,0,0,None])
-    #     return None
-
     def trackingObj(self,pon1,pon2,rad):
         y, x = [pon1[1] +pon2[1]/2,pon1[0] + pon2[0]/2]
         # print x,y,w,h
+        haveline = False
         if len(self.allObj) == 0:
             self.allObj.append([x,y,rad,True,0,0,None])
             return None
-        haveline = False
+
 
         for data in self.allObj:
             if data[0] != None:
@@ -145,11 +107,6 @@ class TrackingObj(object):
                     haveline = True
                     break
         if haveline == False:
-            try:
-                ins = self.allObj.index([None,None,None,False,0,0,None])
-                self.allObj.insert(ins,[x,y,rad,True,0,0,None])
-                self.allObj.remove(self.allObj[ins+1])
-            except Exception as e:
-                self.allObj.append([x,y,rad,True,0,0,None])
+            self.allObj.append([x,y,rad,True,0,0,None])
         return None
 
